549905efc5f81f15c7400b3f58a593d26d7f8b24
LM-VZTECH <117233470+LM-VZTECH@users.noreply.github.com>
2022-12-19 08:12:29 -0500

    [ADD]bision_contact_information

---
+++ bision_contact_information/bision_contact_information/__manifest__.py
@@ -0,0 +1,34 @@
+{
+    'name': "Bision Contact Information",
+
+    'description': """  
+        Add contact information
+    """,
+
+    'author': "DFVZ TECH",
+
+    'contibutors': ['Jean Carlo Gonzalez Martinez'],
+
+    'category': 'Contacts',
+
+    'version': '15.0.0.0',
+
+    'depends': [
+
+        'base',
+        'contacts',
+
+    ],
+
+    'data': [
+
+        # 'views/res_partner.xml'
+
+    ],
+
+    'demo': [
+
+
+
+    ],
+}
