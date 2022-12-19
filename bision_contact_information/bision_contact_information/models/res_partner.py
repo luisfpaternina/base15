154e8710f548e6cee24a49ab9e0146ff4b2d9dce
LM-VZTECH <117233470+LM-VZTECH@users.noreply.github.com>
2022-12-19 08:17:37 -0500

    [UPD]bision_contact_information

--- bision_contact_information/bision_contact_information/models/res_partner.py
+++ bision_contact_information/bision_contact_information/models/res_partner.py
@@ -7,7 +7,7 @@ class ResPartner(models.Model):
 
     _inherit = 'res.partner'
 
-    person_type = fields.Selection(
-       selection=[('legal', 'Persona Juridica y Asimilada'), ('natural', 'Persona Natural y Asimilada')], string='Tipo de Persona')
+    # person_type = fields.Selection(
+    #    selection=[('legal', 'Persona Juridica y Asimilada'), ('natural', 'Persona Natural y Asimilada')], string='Tipo de Persona')
 
-    # campo = fields.Char(string="Campito")
+    campo = fields.Char(string="Campito")
