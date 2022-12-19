dc4a03764c17e71f3bae7f438a2d3dbe9d9dd861
KC-VZTECH <117233767+KC-VZTECH@users.noreply.github.com>
2022-12-19 08:44:33 -0500

    [ADD]partner_commercial_name

---
+++ partner_commercial_name/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Diego Ivan <Github@Diegoivanc>
+# Copyright 2019 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner Commercial Name",
+    "version": "10.0.1.0.0",
+    "summary": "Commercial Name",
+    "category": "Localization",
+    "author": "EXA AUTO PARTS Github@exaap, "
+              "DRACOSOFT Github@Diegoivanc, "
+              "Joan Marín Github@JoanMarin",
+    "website": "https://github.com/odooloco/l10n-colombia",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": [
+        "views/res_partner_views.xml"
+        ],
+    "installable": True,
+}
